a457c05e3640aa9eaefc4d62aad968ab339275a6
Vladyslava <vladyslavav@brokeree.com>
2025-04-10 21:09:33 +0300

    initial commit

---
+++ ManagePassword/Presenter/ManagePasswordForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ManagePassword
+{
+    partial class ManagePasswordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManagePasswordForm));
+			this.dgvDB = new System.Windows.Forms.DataGridView();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.cmAdd = new System.Windows.Forms.ToolStripMenuItem();
+			this.changeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.adminModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.FindBox = new System.Windows.Forms.TextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.dgvDB)).BeginInit();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// dgvDB
+			// 
+			this.dgvDB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dgvDB.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvDB.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.dgvDB.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.dgvDB.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvDB.ContextMenuStrip = this.contextMenuStrip1;
+			this.dgvDB.GridColor = System.Drawing.SystemColors.ControlText;
+			this.dgvDB.Location = new System.Drawing.Point(12, 44);
+			this.dgvDB.Name = "dgvDB";
+			this.dgvDB.Size = new System.Drawing.Size(767, 473);
+			this.dgvDB.TabIndex = 0;
+			this.dgvDB.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvDB_CellMouseClick);
+			this.dgvDB.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvDB_CellMouseDoubleClick);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmAdd,
+            this.changeToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.refreshToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.adminModeToolStripMenuItem,
+            this.infoToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(145, 170);
+			// 
+			// cmAdd
+			// 
+			this.cmAdd.Name = "cmAdd";
+			this.cmAdd.Size = new System.Drawing.Size(144, 22);
+			this.cmAdd.Text = "Add";
+			this.cmAdd.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cmAdd_MouseDown);
+			// 
+			// changeToolStripMenuItem
+			// 
+			this.changeToolStripMenuItem.Name = "changeToolStripMenuItem";
+			this.changeToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+			this.changeToolStripMenuItem.Text = "Change";
+			this.changeToolStripMenuItem.Click += new System.EventHandler(this.changeToolStripMenuItem_Click);
+			// 
+			// deleteToolStripMenuItem
+			// 
+			this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+			this.deleteToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+			this.deleteToolStripMenuItem.Text = "Delete";
+			this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+			// 
+			// refreshToolStripMenuItem
+			// 
+			this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+			this.refreshToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+			this.refreshToolStripMenuItem.Text = "Refresh";
+			this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(141, 6);
+			// 
+			// adminModeToolStripMenuItem
+			// 
+			this.adminModeToolStripMenuItem.Name = "adminModeToolStripMenuItem";
+			this.adminModeToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+			this.adminModeToolStripMenuItem.Text = "Admin mode";
+			this.adminModeToolStripMenuItem.Click += new System.EventHandler(this.adminModeToolStripMenuItem_Click);
+			// 
+			// infoToolStripMenuItem
+			// 
+			this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+			this.infoToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+			this.infoToolStripMenuItem.Text = "Info";
+			this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(141, 6);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// FindBox
+			// 
+			this.FindBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.FindBox.BackColor = System.Drawing.SystemColors.Window;
+			this.FindBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.FindBox.ForeColor = System.Drawing.Color.Silver;
+			this.FindBox.Location = new System.Drawing.Point(12, 18);
+			this.FindBox.Name = "FindBox";
+			this.FindBox.Size = new System.Drawing.Size(767, 20);
+			this.FindBox.TabIndex = 1;
+			this.FindBox.TabStop = false;
+			this.FindBox.Text = "Search";
+			this.FindBox.TextChanged += new System.EventHandler(this.FindBox_TextChanged);
+			this.FindBox.Enter += new System.EventHandler(this.FindBox_Enter);
+			this.FindBox.Leave += new System.EventHandler(this.FindBox_Leave);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 200;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// ManagePasswordForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(791, 529);
+			this.Controls.Add(this.FindBox);
+			this.Controls.Add(this.dgvDB);
+			this.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "ManagePasswordForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "ManagePassword";
+			((System.ComponentModel.ISupportInitialize)(this.dgvDB)).EndInit();
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDB;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cmAdd;
+        private System.Windows.Forms.ToolStripMenuItem changeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem adminModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.TextBox FindBox;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
